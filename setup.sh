4d88b7b15d4d7962bb88ba739ddff332dc14ce00
superRaptor911 <raptor.inc2018@gmail.com>
2021-04-18 23:34:40 +0530

    added setup.sh

--- setup.sh
+++ setup.sh
@@ -10,4 +10,4 @@ read sql_pass
 echo "Enter a secret password of ur choice (will be used as webapp admin pass)"
 read admin_pass
 
-cat database.php | sed "s/my_fcuking_username/$sql_user; s/my_fcuking_db_password/$sql_pass; s/my_fcuking_admin_password" >> database.php
+cat database.php | sed "s/my_fcuking_username/$sql_user/; s/my_fcuking_db_password/$sql_pass/; s/my_fcuking_admin_password/$admin_pass/" >> database.php
