be068338ba56a48f278168110b45fee64cf26a9e
superRaptor911 <raptor.inc2018@gmail.com>
2021-04-21 14:38:40 +0530

    commitsh

--- commit.sh
+++ commit.sh
@@ -3,9 +3,9 @@
 echo "Enter commit message => "
 read msg
 
-mv database.php database.php.old
+mv database.php logs/database.php.old
 cp logs/database.php database.php
 
 git add . && git commit -m "$msg"
 rm database.php
-mv database.php.old database.php
+mv logs/database.php.old database.php
